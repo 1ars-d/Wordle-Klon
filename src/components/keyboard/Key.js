2159c56c6b03b3ada3acd5210ea4ab0c8e65304d
Coder-Lars <71517515+Coder-Lars@users.noreply.github.com>
2022-04-14 21:32:25 +0200

    sd

--- src/components/keyboard/Key.js
+++ src/components/keyboard/Key.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { Transition } from "react-transition-group";
 
 const Key = (props) => {
   return (
-    <Transition in={!props.state} timeout={1200}>
-      {(state) => {
-        return (
-          <button
-            style={{ fontSize: props.enter ? "1.1rem" : "" }}
-            tabIndex="-1"
-            selectable="false"
-            className={`key ${props.large ? "large" : ""} ${
-              state === "exited" ? props.state : ""
-            }`}
-            onClick={() => props.onPress(props.value)}
-          >
-            {props.children}
-          </button>
-        );
-      }}
-    </Transition>
+    <button
+      style={{ fontSize: props.enter ? "1.1rem" : "" }}
+      tabIndex="-1"
+      selectable="false"
+      className={`key ${props.large ? "large" : ""} ${
+        props.state ? props.state : ""
+      }`}
+      onClick={() => props.onPress(props.value)}
+    >
+      {props.children}
+    </button>
   );
 };
 
